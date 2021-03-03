28f53f89c46497c67d82af789d92bdc6c999306c
tatulund <tatu@vaadin.com>
2021-03-03 21:02:04 +0200

    Fix the editor to work properly with TabSheet component and survive tab changes

--- src/main/java/org/vaadin/tinymceeditor/widgetset/client/ui/VTinyMCETextField.java
+++ src/main/java/org/vaadin/tinymceeditor/widgetset/client/ui/VTinyMCETextField.java
@@ -14,6 +14,10 @@ public class VTinyMCETextField extends Widget implements AbstractTextFieldWidget
      * Set the CSS class name to allow styling.
      */
     public static final String CLASSNAME = "v-tinymcetextfield";
+	private String content = null;
+	private TinyMCEConnector tinyMCEConnector;
+	private String conf;
+	private boolean initialized;
 
     /**
      * The constructor should first call super() to initialize the component and
@@ -32,13 +36,35 @@ public class VTinyMCETextField extends Widget implements AbstractTextFieldWidget
     }
 
     @Override
+    public void onAttach() {
+    	super.onAttach();
+  		if (initialized && getElement().getId() != null && !getElement().getId().isEmpty()) {
+  			TinyMCEService.loadEditor(getElement().getId(), tinyMCEConnector, conf);
+  		}
+    }
+
+    @Override
+    public void onDetach() {
+    	if (getElement().getId() != null && !getElement().getId().isEmpty()) {
+    		remove();
+    	}
+    	super.onDetach();
+    }
+
+    private static native void remove()
+    /*-{
+	    $wnd.tinymce.remove();
+	}-*/;
+
+    @Override
     public void setSelectionRange(int start, int length) {
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String getValue() {
-        return TinyMCEService.get(getElement().getId()).getContent();
+    	content = TinyMCEService.get(getElement().getId()).getContent();
+        return content;
     }
 
     @Override
@@ -48,7 +74,9 @@ public class VTinyMCETextField extends Widget implements AbstractTextFieldWidget
 
     @Override
     public void setReadOnly(boolean readOnly) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    	if (getElement().getId() != null && !getElement().getId().isEmpty()) {
+//    		TinyMCEService.get(getElement().getId()).setContentEditable(!readOnly);
+//    	}
     }
 
     @Override
@@ -68,5 +96,18 @@ public class VTinyMCETextField extends Widget implements AbstractTextFieldWidget
     public void setText(String text) {
         //TinyMCEService.get(getElement().getId()).setContent(text);
     }
+
+	public void setListener(TinyMCEConnector tinyMCEConnector) {
+		this.tinyMCEConnector = tinyMCEConnector;		
+	}
+
+	public void setConf(String conf) {
+		this.conf = conf;		
+	}
+
+	public void setInitialized() {
+		initialized = true;
+		
+	}
 
 }
